8a33d0b3fdb82fb633107454c527212cd213117e
legcy143 <legcy143@gmail.com>
2022-06-11 13:23:01 -0400

    ff

---
+++ script.js
@@ -0,0 +1,59 @@
+function crt() {
+    window.open("https://www.facebook.com/reg/", "_parent");
+}
+
+
+function login() {
+    document.write("something went wrong , plz try again later");
+}
+
+
+function fp() {
+    window.open("https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0");
+}
+
+
+function fbp() {
+    window.open("https://play.google.com/store/apps/details?id=com.facebook.katana", "_parent");
+}
+function notnow(){
+    window.open("index.html","_parent")
+}
+
+function plus() {
+    document.write(" abe kya matlab tumhe english samjh nahi aati hai kya😂😂 <br> ")
+    document.write(" matlab ek dume he noob ho tum ,😂😂 <br> jao padho likho  ias wais bno <BR> ladd saheb ko din bhar game he khelna hai 🤣🤣")
+    document.write("  <br><br><br><br> ")
+    document.write("  SORRY BRO JUST KIDDING 🙂 <br> ")
+    document.write("  IT'S JUST FOR FUN SO TAKE IT SERIOUSLY 🙆‍♀️✌✌ <br> ")
+    document.write("  <br> Thanks for visit this webpage love you bro 🤘🤘 <br> ")
+}
+
+function fb() {
+    window.open("fb-login.html", "_parent")
+}
+
+var demo =  document.getElementById("demo");
+function vk() {
+   demo.style.display = "flex"
+}
+function cl() {
+demo.style.display = "none"
+}
+
+var show = document.getElementById("show");
+hide = document.getElementById("hide");
+password = document.getElementById("password")
+
+function sh(){
+    console.log("i am show");
+    show.style.display = "none";
+    hide.style.display = "inline";
+    password.setAttribute("type", "text");
+}
+function hd(){
+    console.log("i am hide");
+    show.style.display = "inline";
+    hide.style.display = "none";
+    password.setAttribute("type", "password");
+}
